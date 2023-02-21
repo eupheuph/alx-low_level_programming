0c4bca0b18dfd42aa568e6997ca9763dbd3a9fec
Euph <euphmosera@gmail.com>
2023-02-21 12:53:11 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,14 +5,14 @@
  * Return: Always 0
 */
 
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char c;
-	for (c ='a'; c <='z'; c++);
-	{
-		putchar(c);
-	}
-	putchar('\n');
-	return (0);
+
+	for (c ='a'; c <='z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
 }
 
