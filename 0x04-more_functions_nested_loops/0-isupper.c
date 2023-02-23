d2f6740864de8774dee86bda0758e79b85220636
Euph <euphmosera@gmail.com>
2023-02-23 13:13:34 -0800

    isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_isupper -uppercase letter
+ *Checks for uppercase characters
+ *Return 1 or 0
+*/
+
+int _isupper(int c)
+{
+	int c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
