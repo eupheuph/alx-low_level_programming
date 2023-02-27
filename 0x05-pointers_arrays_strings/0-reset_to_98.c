6d26f90a668020835f375bb42dd65c8b6e0cafd2
Euph <euphmosera@gmail.com>
2023-02-27 08:27:14 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,13 +3,13 @@
 /**
  *reset_to_98 - takes a pointer to an int parameter
  *sets the value to 98
+ *@n: input
  *return: n
 */
 
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 
-	return (0);
 }
 
