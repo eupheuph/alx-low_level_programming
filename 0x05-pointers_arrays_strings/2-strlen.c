fcf1f823174ba74380a133cece84b737fbbc06b3
Euph <euphmosera@gmail.com>
2023-02-27 09:04:56 -0800

    value length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_strlen - returns the length of a string
+ *@s: input
+ *Return: length
+*/
+
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
