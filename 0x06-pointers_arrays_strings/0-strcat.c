45a3ed72b1f978016e12acb66a72d91fa65df91e
Euph <euphmosera@gmail.com>
2023-03-01 10:41:14 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to
+ * @dest: the destiation string to be concatenated upon
+ *
+ * Return:pointer to the resulting string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int index = 0;
+	int dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+	return (dest);
+}
