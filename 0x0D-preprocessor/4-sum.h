0f4ab563d5b9ee63e69c5646f0ded5dcad24816c
Euph <euphmosera@gmail.com>
2023-03-20 11:53:56 -0700

    SUM

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/*
+ * File: 4-sum.h
+ * Desc: Defines a function-like macro SUM(x, y)
+ *       that calculates the sum of  x and y.
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
