cf67affb15655b37c8fabc3651ad2691977c2e4c
Euph <euphmosera@gmail.com>
2023-03-20 11:59:16 -0700

    Object-like Macro

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,9 +1,12 @@
-#ifndef SIZE 
-#define SIZE 1024
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
 
-/**
- *file: size 
- *desc: defines macro with an abbreviation
+/*
+ * File: 0-object_like_macro.h
+ * Desc: Defines a macro named SIZE of value 1024
  */
 
+#define SIZE 1024
+
 #endif
+
