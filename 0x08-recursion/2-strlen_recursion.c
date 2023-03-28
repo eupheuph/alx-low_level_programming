fa0bf0de7bebb0703b873245634b0d9930db4daa
eupheuph <euphmosera@gmail.com>
2023-03-28 14:38:08 -0700

    returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function declaration
+ * @s: the string
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s);
+
+int main(void)
+{
+	_strlen_recursion("hello world");
+}
+
+/**
+ * _strlen_recursion - function definition
+ * @s: the string to measure
+ * Description: finds the length of a string
+ * using recursion
+ * Return: the string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s)
+	{
+		length = _strlen_recursion(s + 1);
+		return (length = length + 1);
+	}
+	return (0);
+}
