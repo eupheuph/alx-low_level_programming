744581aefeea82bf97662a3f38b979503c9fe646
eupheuph <euphmosera@gmail.com>
2023-06-12 13:08:55 -0700

    whats my name

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+int main(int argc, char *argv[]);
+
+#endif
